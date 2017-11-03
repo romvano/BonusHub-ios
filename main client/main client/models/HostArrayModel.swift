d8e96ef81f985bde02c10c4705e090510c8efd7f
Ivan <romvano@gmail.com>
2017-11-03 21:42:23 +0300

    host model, get host api, list hosts api

---
+++ main client/main client/models/HostArrayModel.swift
@@ -0,0 +1,21 @@
+//
+//  HostArrayModel.swift
+//  main client
+//
+//  Created by Admin on 03.11.17.
+//  Copyright © 2017 Daria Firsova. All rights reserved.
+//
+
+import UIKit
+
+class HostArrayModel {
+    var hostArray: NSMutableArray
+    
+    init (arr: NSMutableArray) {
+        self.hostArray = arr
+    }
+    
+    func saveLocally() {
+        
+    }
+}
--- main client/main client/models/HostModel.swift
+++ main client/main client/models/HostModel.swift
@@ -16,22 +16,22 @@ class HostModel {
     var latitude: Float64?
     var longitude: Float64?
     var offer: String?
-    var timeOpen: NSDate?
-    var timeClose: NSDate?
+    var timeOpen: Date?
+    var timeClose: Date?
     var profileImage: NSURL?
     var points: Float
     var loyalityType: Int
     var loyalityParam: Int
     
-    init? (uid: String, title: String, description: String?, address: String?,
+    init? (uid: String?, title: String?, description: String?, address: String?,
            latitude: Float64?, longitude: Float64?, offer: String?,
-           timeOpen: NSDate?, timeClose: NSDate?, profileImage: NSURL?,
-           points: Float, loyalityType: Int, loyalityParam: Int) {
-        guard isObjectId(s: uid) else {
+           timeOpen: Date?, timeClose: Date?, profileImage: NSURL?,
+           points: Float?, loyalityType: Int?, loyalityParam: Int?) {
+        guard isObjectId(s: uid), title != nil, points != nil, loyalityType != nil, loyalityParam != nil else {
             return nil
         }
-        self.uid = uid
-        self.title = title
+        self.uid = uid!
+        self.title = title!
         self.description = description
         self.address = address
         self.latitude = latitude
@@ -40,8 +40,12 @@ class HostModel {
         self.timeOpen = timeOpen
         self.timeClose = timeClose
         self.profileImage = profileImage
-        self.points = points
-        self.loyalityType = loyalityType
-        self.loyalityParam = loyalityParam
+        self.points = points!
+        self.loyalityType = loyalityType!
+        self.loyalityParam = loyalityParam!
+    }
+    
+    func saveLocally() {
+        
     }
 }
