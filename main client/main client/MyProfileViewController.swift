6e88dee9eba6d171180ae787524dcf37cdfe5290
Ivan <romvano@gmail.com>
2017-11-23 03:41:58 +0300

    started to do a MyProfileViewController

--- main client/main client/MyProfileViewController.swift
+++ main client/main client/MyProfileViewController.swift
@@ -13,10 +13,39 @@ class MyProfileViewController: UIViewController {
     @IBOutlet weak var QRCodeImageView: UIImageView!
     @IBOutlet weak var IdLabel: UILabel!
     
+    private func onGetInfoResult (code: Int?, user: UserModel?) {
+        if code == API.OK && user != nil {
+            self.render(id: user!.uid)
+            return
+        }
+        
+        if code == API.NOT_AUTHED {
+            // переход на экран логина + очистка кеша (cleanRealm())
+            
+            cleanRealm()
+            return
+        }
+        
+        // if not 200 and not 401 - try to load from local storage
+        let loadedUser: UserModel? = UserModel.load()
+        if loadedUser != nil {
+            self.render(id: loadedUser!.uid)
+            return
+        }
+        
+        // if local storage doesn't contain a user model - login screen
+        // aпереход на экран логина + очистка кеша
+        
+        cleanRealm()
+    }
+    
+    private func render(id: String) {
+        IdLabel.text = id
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        ClientAPI().getClientInfo(onResult: self.onGetInfoResult) // aхер с ним, пусть будет так
     }
 
     override func didReceiveMemoryWarning() {
